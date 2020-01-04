6fbbcdb7ec1bd71a33ec3850bd010487d9b0e318
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-04 00:34:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,7 +10,7 @@ end
 def find_max_value(array)
   x = 0 
   array.length.times { |index| x = array[index] if array.[index] > x 
-  x = array.[index]
+  x = array[index]
 end
 }
 x
@@ -19,7 +19,7 @@ end
 def find_min_value(array)
   x = 0 
   array.length.times { |index| x = array[index] if array.[index] < x 
-  x = array.[index]
+  x = array[index]
 end
 }
 x
